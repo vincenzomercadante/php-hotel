f68d8b3fc0dd8177920480d43a948bd8d5608a83
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-02-26 15:55:02 +0100

    feat: add bootstrap

--- index.php
+++ index.php
@@ -13,11 +13,20 @@ require_once('./main.php');
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP Hotel</title>
+
+        <!-- import bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+        <!-- my style -->
+        <link href="./style.css" rel="stylesheet">
+
     </head>
-    <body>
-        <h1>HOTEL BOL VA GO</h1>
-        <table border='1'>
+    <body class="d-flex justify-content-center flex-column px-3">
+        <!-- page title -->
+        <h1 class="text-center text-success display-3 my-5">HOTEL BOL VA GO</h1>
+        <table class="table table-hover table-dark">
             <theader>
+                <!-- table header -->
                 <tr>
                     <th>Name</th>
                     <th>Description</th>
@@ -27,11 +36,12 @@ require_once('./main.php');
                 </tr>
             </theader>
             <tbody>
+                <!-- hotel info printer -->
                 <?php foreach($hotels as $hotel): ?>
                 <tr>
                     <td><?= $hotel['name'] ?></td>
                     <td><?= $hotel['description'] ?></td>
-                    <td><?= $hotel['parking'] ?></td>
+                    <td><?= ($hotel['parking'] ? 'Yes' : 'No') ?></td>
                     <td><?= $hotel['vote'] ?></td>
                     <td><?= $hotel['distance_to_center'] ?></td>
                 </tr>
